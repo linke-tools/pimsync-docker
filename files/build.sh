4add8726196ac9a3d74e03e94de440f09ca5023a
root <root@horologium.ari.uni-heidelberg.de>
2026-04-22 14:35:02 +0200

    Upgrade to pimsync 0.5.7

--- files/build.sh
+++ files/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PIMSYNC_VERSION="v0.5.7"
+
 # Install required packages
 apt-get update
-apt-get -y install ca-certificates curl git make libsqlite3-dev libsqlite3-0 build-essential scdoc gettext
+apt-get -y install ca-certificates curl git make libsqlite3-dev libsqlite3-0 build-essential scdoc gettext pkg-config
 
 # Install rust
 cd ~
@@ -10,8 +12,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 
 # Build and install pimsync
-git clone https://git.sr.ht/~whynothugo/pimsync pimsync
+git clone --branch "$PIMSYNC_VERSION" --depth 1 https://git.sr.ht/~whynothugo/pimsync pimsync
 cd pimsync
+git submodule update --init --recursive
+
 make build
 make install
 
